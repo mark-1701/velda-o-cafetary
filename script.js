58aec7ffe6c94f97d424652bf19610ff2fc42215
mark-1701 <vini.muralles@gmail.com>
2025-11-08 18:08:50 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const navLinks= document.querySelectorAll(".nav-menu .nav-link");
+
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () =>{
+//Toggle mobile menu visibility
+document.body.classList.toggle("show-mobile-menu");
+});
+
+// close menu when the close button is clicked
+menuCloseButton.addEventListener("click", () => menuOpenButton.click
+());
+
+//close menu when the nav link is cliked
+navLinks.forEach(link =>{
+link.addEventListener("click", () => menuOpenButton.click());
+});
+
+
+//initializer swiper
+const swiper = new Swiper('.slider-wrapper', {
+loop: true,
+grabCursor: true,
+spaceBetween: 25,
+
+  // If we need pagination
+pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dinamicBullets: true,
+},
+
+  // Navigation arrows
+navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+},
+
+//responsive breakpoint
+breakpoints:{
+    0:{
+    slidesPerView: 1
+    },
+    768:{
+    slidesPerView: 2
+    },
+    1024:{
+    slidesPerView: 3
+    },
+}
+
+});
+
